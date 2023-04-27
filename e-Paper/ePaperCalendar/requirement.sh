169c9beba1505cafc4943c657b32051a6b41518d
BoLin <jerry084@yahoo.com.tw>
2023-04-27 15:04:24 +0800

    Add requirement bash script

---
+++ e-Paper/ePaperCalendar/requirement.sh
@@ -0,0 +1,13 @@
+#update system
+sudo apt-get update
+sudo apt-get upgrade
+
+#get e-paper display requirement
+sudo apt-get install python3-pip
+sudo apt-get install python3-pil
+sudo apt-get install python3-numpy
+sudo pip3 install RPi.GPIO
+sudo pip3 install spidev
+
+#ics requirement
+sudo pip install ics
