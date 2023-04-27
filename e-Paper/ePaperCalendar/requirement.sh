7008cfd150f8f2ea48641ccab006de39e05ce065
BoLin <jerry084@yahoo.com.tw>
2023-04-27 15:25:48 +0800

    Update requirement

--- e-Paper/ePaperCalendar/requirement.sh
+++ e-Paper/ePaperCalendar/requirement.sh
@@ -3,11 +3,18 @@ sudo apt-get update
 sudo apt-get upgrade
 
 #get e-paper display requirement
-sudo apt-get install python3-pip
-sudo apt-get install python3-pil
-sudo apt-get install python3-numpy
-sudo pip3 install RPi.GPIO
-sudo pip3 install spidev
+sudo apt-get pip
+pip install pillow
+pip install numpy
+pip install RPi.GPIO
+pip install spidev
 
 #ics requirement
-sudo pip install ics
+pip install ics
+pip install arrow
+pip install requests
+
+#make config file
+mkdir config
+echo > config/calUrls.cfg
+echo > config/weatherConfig.json
