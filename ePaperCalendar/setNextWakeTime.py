b5250e43e651cc3def7d72e2980e33e6b262c840
Windows at ZenBook Pro <jerry084@yahoo.com.tw>
2023-05-10 20:19:48 +0800

    test1

--- ePaperCalendar/setNextWakeTime.py
+++ ePaperCalendar/setNextWakeTime.py
@@ -1,10 +1,21 @@
 import json
+from datetime import timedelta
+from datetime import datetime
+from dateutil import parser
 
 def main():
     with open('/etc/pisugar-server/config.json', 'r') as file:
         content = file.read()
         data = json.loads(content)
-        print(data)
+    
+    oldWakeTime = parser.parse(data['auto_wake_time'])
+    now = datetime.now()
+    print(newWakeTime)
+    #if oldWakeTime.hour==11:
+    now = now+timedelta(days=1)
+    newWakeTime = datetime(now.year, now.month, now.day, 0, 0, 0, 0, now.timetz)
+    print(newWakeTime)
+
 
 if __name__=='__main__':
     main()
