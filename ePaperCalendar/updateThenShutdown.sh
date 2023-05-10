84c448ce04af5dc6122b3db2a8c26a91d6aeb0c8
Windows at ZenBook Pro <jerry084@yahoo.com.tw>
2023-05-10 21:16:27 +0800

    update bash

--- ePaperCalendar/updateThenShutdown.sh
+++ ePaperCalendar/updateThenShutdown.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-python /home/k2345777/ePaper-Google-Calendar/ePaperCalendar/setNextWakeTime.py
-
-systemctl restart pisugar-server
-
 while ! ping -c 1 -W 1 www.google.com; do
     sleep 1
 done
 
-python -m pip install arrow
-python -m pip install ics
+echo "$(date)" >> /home/k2345777/batteryLog.txt
+echo "get battery" | nc -q 0 127.0.0.1 8423 >> /home/k2345777/batteryLog.txt
+
+python /home/k2345777/ePaper-Google-Calendar/ePaperCalendar/setNextWakeTime.py
+systemctl restart pisugar-server
+
+#python -m pip install arrow
+#python -m pip install ics
 
 python /home/k2345777/ePaper-Google-Calendar/ePaperCalendar/updateCalendar.py
 
