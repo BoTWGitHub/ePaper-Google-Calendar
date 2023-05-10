f9639e951a238c6791deb2d82da8af71c823dff6
Windows at ZenBook Pro <jerry084@yahoo.com.tw>
2023-05-10 20:45:38 +0800

    update bash

--- ePaperCalendar/updateThenShutdown.sh
+++ ePaperCalendar/updateThenShutdown.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-python /home/k2345777/ePaper-Google-Calendar/ePaperCalendar/setNextWakeTime.py
+sudo python /home/k2345777/ePaper-Google-Calendar/ePaperCalendar/setNextWakeTime.py
 
-systemctl restart pisugar-server
+sudo systemctl restart pisugar-server
 
 while ! ping -c 1 -W 1 www.google.com; do
     sleep 1
