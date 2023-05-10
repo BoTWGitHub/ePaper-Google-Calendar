e56b947a2aeb057574eae1f27ec9002f6186dbaa
Windows at ZenBook Pro <jerry084@yahoo.com.tw>
2023-05-10 20:04:47 +0800

    runnable script

--- ePaperCalendar/updateThenShutdown.sh
+++ ePaperCalendar/updateThenShutdown.sh
@@ -1,14 +1,9 @@
-max_wait = 180
-count = 0
+#!/bin/bash
 
 while ! ping -c 1 -W 1 www.google.com; do
-    count = $((count+1))
-    if [ $count -ge $max_wait ]; then
-        break
-    fi
     sleep 1
 done
 
 python /home/k2345777/ePaper-Google-Calendar/ePaperCalendar/updateCalendar.py
 
-sudo shutdown now
+#sudo shutdown -h 5
