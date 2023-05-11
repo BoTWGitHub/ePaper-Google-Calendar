f1cafa3a65037f2b0db7f0f973eff0bedfe290a2
Windows at ZenBook Pro <jerry084@yahoo.com.tw>
2023-05-11 21:19:21 +0800

    Update bash waiting pisugar server

--- ePaperCalendar/updateThenShutdown.sh
+++ ePaperCalendar/updateThenShutdown.sh
@@ -13,18 +13,45 @@ while [ $retry -gt 0 ]; do
     fi
 done
 
-if [ $connect == 1 ]; then
+#python -m pip install arrow
+#python -m pip install ics
+echo "update calendar..."
+python /home/k2345777/ePaper-Google-Calendar/ePaperCalendar/updateCalendar.py
+
+echo "update wakeup time"
+python /home/k2345777/ePaper-Google-Calendar/ePaperCalendar/setNextWakeTime.py
+systemctl restart pisugar-server
+# Pisugar server 的 URL
+pisugar_server_url="http://localhost:8421"
+# 最大重試次數
+retry=18
+# 間隔時間（秒）
+interval=10
+serverReady=0
+
+while [ $retry -gt 0 ]; do
+    # 嘗試訪問 Pisugar server
+    response=$(curl -s -o /dev/null -w "%{http_code}" "${pisugar_server_url}")
+
+    # 檢查響應代碼，200 表示 Pisugar server 已經初始化完成
+    if [ "$response" -eq 200 ]; then
+        echo "Pisugar server 已初始化完成"
+        serverReady=1
+        break
+    else
+        retry=$((retry - 1))
+    fi
+
+    # 等待一段時間後重試
+    echo "等待 ${interval} 秒後再次嘗試..."
+    sleep "$interval"
+done
+
+if [ $serverReady == 1 ]; then
     echo "$(date)" >> /home/k2345777/batteryLog.txt
+    ret="get battery" | nc -q 0 127.0.0.1 8423
     echo "get battery" | nc -q 0 127.0.0.1 8423 >> /home/k2345777/batteryLog.txt
     echo "rtc_web" | nc -q 0 127.0.0.1 8423
 fi
 
-#python /home/k2345777/ePaper-Google-Calendar/ePaperCalendar/setNextWakeTime.py
-#systemctl restart pisugar-server
-
-#python -m pip install arrow
-#python -m pip install ics
-
-python /home/k2345777/ePaper-Google-Calendar/ePaperCalendar/updateCalendar.py
-
-sudo shutdown -h 3
+sudo shutdown now
